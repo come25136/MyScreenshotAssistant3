1747d427e3e3024895deaaaf11c753cc1d079d5a
come25136 <come25136@gmail.com>
2017-08-09 19:11:34 +0900

    [update] バージョン情報更新

--- MyScreenshotAssistant3/Properties/AssemblyInfo.cs
+++ MyScreenshotAssistant3/Properties/AssemblyInfo.cs
@@ -51,7 +51,7 @@ using System.Windows;
 // すべての値を指定するか、下のように '*' を使ってビルドおよびリビジョン番号を 
 // 既定値にすることができます:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.0.0")]
-[assembly: AssemblyFileVersion("3.1.0.0")]
+[assembly: AssemblyVersion("3.1.1.0")]
+[assembly: AssemblyFileVersion("3.1.1.0")]
 [assembly: NeutralResourcesLanguage("ja-JP")]
 
